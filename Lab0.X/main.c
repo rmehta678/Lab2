6113bbbf32747a79b72f539769ce3ac1e58a43df
rmehta678 <rmehta678@gmail.com>
2015-09-14 23:25:40 -0700

    Testing for commit functionality.

--- Lab0.X/main.c
+++ Lab0.X/main.c
@@ -75,7 +75,7 @@ void DelayMs(unsigned int msec)
 	unsigned int tWait, tStart;
 		
     tWait=(SYS_FREQ/2000)*msec;
-    tStart=ReadCoreTimer();
-    while((ReadCoreTimer()-tStart)<tWait);		// wait for the time to pass
+//    tStart=ReadCoreTimer(); //Find what this is
+//    while((ReadCoreTimer()-tStart)<tWait);		// wait for the time to pass
 
 }
